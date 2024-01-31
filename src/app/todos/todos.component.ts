6e41ba442dcf72292fcf9e5d00ee5c467d2e76d6
michaelwoelfel <woelfelm@t-online.de>
2024-01-31 18:25:15 +0700

    load todos from backend

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
-import {  MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Component, NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { environment } from '../../environments/environment.prod';
+import { lastValueFrom } from 'rxjs';
+import { CommonModule, NgFor, NgForOf } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-todos',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    NgFor,
+
+
+
   ],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent {
+  constructor(public httpClient: HttpClient) {
+
+  }
+  todos: any = [];
+
+  async ngOnInit() {
+    this.todos = await this.loadTodos();
+    console.log(this.todos);
+  }
+  
+
+  loadTodos() {
+    const url = environment.baseUrl + 'todos/';
+    return lastValueFrom(this.httpClient.get(url));
+  }
 
 }
